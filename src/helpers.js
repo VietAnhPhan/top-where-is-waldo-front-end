7cad6efb8de0189c1cc081785e40e833a089534a
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-10-06 21:56:30 +0700

    Create helper function - create getFullDate function with format of full date

---
+++ src/helpers.js
@@ -0,0 +1,67 @@
+const date = new Date();
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const daysWithZero = [
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25",
+  "26",
+  "27",
+  "28",
+  "29",
+  "30",
+  "31",
+];
+function getFullDate() {
+  const fullDate =
+    monthNames[date.getMonth()] +
+    " " +
+    daysWithZero[date.getDate() - 1] +
+    ", " +
+    date.getFullYear() +
+    " " +
+    date.getHours() +
+    ":" +
+    date.getMinutes() +
+    ":" +
+    date.getSeconds();
+
+  return fullDate;
+}
+
+export { getFullDate };
