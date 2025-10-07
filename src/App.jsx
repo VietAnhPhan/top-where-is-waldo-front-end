8e0515bf573a1173e3019a889115fd98b75caa4e
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-10-07 21:24:39 +0700

    Refactor App component to check if markers exist to prevent loop of rerender

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,27 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
-  console.log(user);
+ 
   useEffect(() => {
-    async function fetchMarkers() {
-      const response = await fetch(
-        `http://localhost:3000/moves?gameplayId=${Number(
-          user.gameplayId
-        )}&userId=${Number(user.userId)}`
-      );
+    if (!user.markers) {
+      async function fetchMarkers() {
+        const response = await fetch(
+          `http://localhost:3000/moves?gameplayId=${Number(
+            user.gameplayId
+          )}&userId=${Number(user.userId)}`
+        );
 
-      if (response.ok) {
-        const markers = await response.json();
-        setUser({
-          ...user,
-          markers: markers,
-        });
+        if (response.ok) {
+          const markers = await response.json();
+          setUser({
+            ...user,
+            markers: markers,
+          });
+        }
       }
+      fetchMarkers();
     }
-    fetchMarkers();
-  }, []);
+  });
 
   return (
     <>
