18b5c023e1cf75cc6436aa9a9a3e0333794cf0bf
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-10-07 21:11:00 +0700

    Add useEffect to fetch markers on rendering App

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,30 @@ import "./App.css";
 import { RouterProvider } from "react-router";
 import router from "./routes";
 import { AuthContext } from "./Context";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+  console.log(user);
+  useEffect(() => {
+    async function fetchMarkers() {
+      const response = await fetch(
+        `http://localhost:3000/moves?gameplayId=${Number(
+          user.gameplayId
+        )}&userId=${Number(user.userId)}`
+      );
+
+      if (response.ok) {
+        const markers = await response.json();
+        setUser({
+          ...user,
+          markers: markers,
+        });
+      }
+    }
+    fetchMarkers();
+  }, []);
+
   return (
     <>
       <AuthContext value={{ user, setUser }}>
