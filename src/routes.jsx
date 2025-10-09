f798a05c5bdc51fc18ae40c1de059d9c104756b8
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-10-09 17:38:54 +0700

    Refactor routes to add error handling

--- src/routes.jsx
+++ src/routes.jsx
@@ -12,16 +12,20 @@ const router = createBrowserRouter([
     element: <Gameboard sitename="Where is Waldo" />,
     loader: async () => {
       const user = JSON.parse(localStorage.getItem("user"));
+      try {
+        const response = await fetch(
+          `http://localhost:3000/moves?gameplayId=${user.gameplayId}&userId=${user.userId}`
+        );
 
-      const response = await fetch(
-        `http://localhost:3000/moves?gameplayId=${user.gameplayId}&userId=${user.userId}`
-      );
-
-      if (response.ok) {
-        const markers = await response.json();
-        return markers;
+        if (response.ok) {
+          const markers = await response.json();
+          return markers;
+        }
+      } catch (err) {
+        console.log(`There is ${err}`);
       }
     },
+    errorElement: <p>There is an error</p>,
   },
 ]);
 
