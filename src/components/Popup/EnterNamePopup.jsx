766d49367bede0aa2d4a22f5718328a844004f2d
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-10-11 13:02:54 +0700

    Handle enter name for EnterNamePopup

--- src/components/Popup/EnterNamePopup.jsx
+++ src/components/Popup/EnterNamePopup.jsx
@@ -1,15 +1,38 @@
-import { useRef } from "react";
+import { useContext, useState } from "react";
 import "./EnterNamePopup.css";
+import { useNavigate } from "react-router";
+import { AuthContext } from "../../Context";
 
-const EnterNamePopup = ({dialogRef}) => {
-//   const dialogRef = useRef(null);
-
-//   function handleOpen() {
-//     dialogRef.current.showModal();
-//   }
+const EnterNamePopup = ({ dialogRef }) => {
+  const auth = useContext(AuthContext);
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
 
   function handleClose() {
     dialogRef.current.close();
+  }
+
+  async function handleEnter() {
+    try {
+      const userResponse = await fetch(
+        `http://localhost:3000/users/${auth.user.userId}`,
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            id: auth.user.userId,
+            name: name,
+          }),
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+      if (userResponse.ok) {
+        navigate("/leader-board");
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
@@ -19,10 +42,21 @@ const EnterNamePopup = ({dialogRef}) => {
           Close
         </button>
         <div className="p-40 flex flex-col">
-            <p className="text-5xl">Congratulate!</p>
-            <p className="mb-6">You complete this round</p>
-            <input type="text" placeholder="Enter your name..." className="border rounded w-full py-2 px-3 mb-6"/>
-            <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Enter</button>
+          <p className="text-5xl">Congratulate!</p>
+          <p className="mb-6">You complete this round</p>
+          <input
+            type="text"
+            placeholder="Enter your name..."
+            className="border rounded w-full py-2 px-3 mb-6"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button
+            type="button"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleEnter}
+          >
+            Enter
+          </button>
         </div>
       </dialog>
 
